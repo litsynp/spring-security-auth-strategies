c500ad9ef113d8111025799c372883b107087e0d
litsynp <nocte55is@gmail.com>
2022-06-14 03:32:43 +0900

    feat: add WIP security config

---
+++ springsec-session/src/main/java/com/litsynp/springsecsession/global/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.litsynp.springsecsession.global.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().permitAll())
+                .httpBasic().disable();
+        return http.build();
+    }
+}
