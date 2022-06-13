854a31b25329c688db28c5c46fe638ef5acc7384
litsynp <nocte55is@gmail.com>
2022-06-14 03:23:27 +0900

    feat: add BaseTimeEntity

--- springsec-session/src/main/java/com/litsynp/springsecsession/SessionApplication.java
+++ springsec-session/src/main/java/com/litsynp/springsecsession/SessionApplication.java
@@ -2,7 +2,9 @@ package com.litsynp.springsecsession;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SessionApplication {
 
