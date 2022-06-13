ee2332d6758cc3e38321de35402b15333ff496c0
litsynp <nocte55is@gmail.com>
2022-06-14 01:20:26 +0900

    build: init project

---
+++ springsec-session/src/test/java/com/litsynp/springsecsession/SessionApplicationTests.java
@@ -0,0 +1,17 @@
+package com.litsynp.springsecsession;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class SessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
