d1b14ae26cd07fc8f36c7a7b2c34b1eee4541ea8
litsynp <nocte55is@gmail.com>
2022-06-15 00:20:59 +0900

    fix: change status code for member not found in post API

--- springsec-session/src/main/java/com/litsynp/springsecsession/domain/post/exception/PostMemberNotFoundException.java
+++ springsec-session/src/main/java/com/litsynp/springsecsession/domain/post/exception/PostMemberNotFoundException.java
@@ -3,7 +3,7 @@ package com.litsynp.springsecsession.domain.post.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
 public class PostMemberNotFoundException extends RuntimeException {
 
     public PostMemberNotFoundException(Long id) {
