83889d187b0eacec6b52eee97f7bfe571b330350
litsynp <nocte55is@gmail.com>
2022-06-15 00:21:35 +0900

    refactor: remove unused import

--- springsec-session/src/test/java/com/litsynp/springsecsession/SessionApplicationTests.java
+++ springsec-session/src/test/java/com/litsynp/springsecsession/SessionApplicationTests.java
@@ -1,7 +1,5 @@
 package com.litsynp.springsecsession;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
