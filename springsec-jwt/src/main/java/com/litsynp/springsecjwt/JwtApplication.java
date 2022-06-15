216be1f446398f5ad64ae7d5c0e16a9f81f7a478
litsynp <nocte55is@gmail.com>
2022-06-16 01:39:04 +0900

    build: add modules

---
+++ springsec-jwt/src/main/java/com/litsynp/springsecjwt/JwtApplication.java
@@ -0,0 +1,12 @@
+package com.litsynp.springsecjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtApplication.class, args);
+    }
+}
