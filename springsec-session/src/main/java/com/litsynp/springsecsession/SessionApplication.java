3fd7ff1e365a76fc566e572548a0dc5382dc0e53
litsynp <nocte55is@gmail.com>
2022-06-15 21:20:32 +0900

    refactor: create JpaAuditingConfig

--- springsec-session/src/main/java/com/litsynp/springsecsession/SessionApplication.java
+++ springsec-session/src/main/java/com/litsynp/springsecsession/SessionApplication.java
@@ -2,9 +2,7 @@ package com.litsynp.springsecsession;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class SessionApplication {
 
