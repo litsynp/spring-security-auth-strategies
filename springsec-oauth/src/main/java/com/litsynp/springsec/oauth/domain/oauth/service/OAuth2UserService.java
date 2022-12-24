c89dac34f81c054e79d01170bc4c5151514cff13
litsynp <nocte55is@gmail.com>
2022-12-24 20:15:34 +0900

    chore: apply transaction to member loading

--- springsec-oauth/src/main/java/com/litsynp/springsec/oauth/domain/oauth/service/OAuth2UserService.java
+++ springsec-oauth/src/main/java/com/litsynp/springsec/oauth/domain/oauth/service/OAuth2UserService.java
@@ -17,6 +17,7 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +27,7 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
     private final MemberRepository memberRepository;
 
     @Override
+    @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User user = super.loadUser(userRequest);
 
